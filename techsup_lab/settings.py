03448b39933dbcc81dcb2c8db03d5edc5b955c09
aamihailov <aamihailov@aamihailov-VirtualBox.(none)>
2012-10-07 15:43:34 +0700

    database relocated to mysql server

--- techsup_lab/settings.py
+++ techsup_lab/settings.py
@@ -12,11 +12,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'F:/sqlite.db',                    # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'techsup',                    # Or path to database file if using sqlite3.
+        'USER': 'techsup',                    # Not used with sqlite3.
+        'PASSWORD': '123123',                 # Not used with sqlite3.
+        'HOST': 'amihailov.pro',              # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
