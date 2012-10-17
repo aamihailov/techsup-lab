7baa10f4dba3f9fe2d8a121f2fb68f008b07045a
anastass <anastass@anastass-Aspire-5310>
2012-10-18 01:46:58 +0700

    add: procedures 'add_equipment' and 'add_equipment_with_date'

--- common_data/sql_queries/new_queries.sql
+++ common_data/sql_queries/new_queries.sql
@@ -897,4 +897,96 @@ END$$
 
 -----------------------------------------------------------------------------------------------
 
+DROP PROCEDURE IF EXISTS add_equipment;
+CREATE PROCEDURE add_equipment(
+                               IN in_name            VARCHAR( 128 ),
+                               IN in_serial_number   VARCHAR( 128 ),
+                               IN in_addr            VARCHAR( 256 ),
+                               IN in_equipment_model VARCHAR( 128 )                               
+                              )
+BEGIN
+
+START TRANSACTION;
+
+-- добавление оборудования
+
+INSERT INTO equipment ( name, serial_number, addr, equipment_model_id )
+    VALUES (
+        in_name,
+        in_serial_number,
+        in_addr,
+        (
+            SELECT id
+            FROM equipment_model
+            WHERE LOWER( equipment_model.name ) LIKE in_equipment_model
+        )
+    );
+
+INSERT INTO equipment_operation ( datetime, equipment_id, eq_oper_type_id )
+    VALUES (
+            ( SELECT NOW() ),
+            (
+                SELECT id
+                FROM equipment
+                WHERE LOWER( equipment.serial_number ) LIKE in_serial_number
+            ),
+            (
+                SELECT id
+                FROM equipment_operation_type
+                WHERE LOWER( equipment_operation_type.name ) = 'поступление'
+            )
+   );
+
+COMMIT;
+
+END$$
+
+-----------------------------------------------------------------------------------------------
+
+DROP PROCEDURE IF EXISTS add_equipment_with_date;
+CREATE PROCEDURE add_equipment_with_date(
+                               IN in_name            VARCHAR( 128 ),
+                               IN in_serial_number   VARCHAR( 128 ),
+                               IN in_addr            VARCHAR( 256 ),
+                               IN in_equipment_model VARCHAR( 128 ),
+			       IN in_datetime DATETIME                               
+                              )
+BEGIN
+
+START TRANSACTION;
+
+-- добавление оборудования
+
+INSERT INTO equipment ( name, serial_number, addr, equipment_model_id )
+    VALUES (
+        in_name,
+        in_serial_number,
+        in_addr,
+        (
+            SELECT id
+            FROM equipment_model
+            WHERE LOWER( equipment_model.name ) LIKE in_equipment_model
+        )
+    );
+
+INSERT INTO equipment_operation ( datetime, equipment_id, eq_oper_type_id )
+    VALUES (
+            in_datetime,
+            (
+                SELECT id
+                FROM equipment
+                WHERE LOWER( equipment.serial_number ) LIKE in_serial_number
+            ),
+            (
+                SELECT id
+                FROM equipment_operation_type
+                WHERE LOWER( equipment_operation_type.name ) = 'поступление'
+            )
+   );
+
+COMMIT;
+
+END$$
+
+-----------------------------------------------------------------------------------------------
 
