1968658d674b3c8516888220dfcb3e9d96d13bdb
anastass <anastass@anastass-Aspire-5310>
2012-10-18 22:40:24 +0700

    model 'EquipmentOwner' was corrected

--- techsup_run/models/EquipmentOwner.py
+++ techsup_run/models/EquipmentOwner.py
@@ -2,13 +2,15 @@
 
 from django.db import models
 
+import settings as s
+
 # Связь сотрудника и оборудования
 class EquipmentOwner(models.Model):
     equipment = models.ForeignKey('Equipment')
-    employee  = models.ManyToManyField('Employee')
+    employee  = models.ForeignKey('Employee')
      
     class Meta:
         app_label = 'techsup_run'
-        db_table = 'equipment_owner'
-        unique_together = ('equipment_id', 'employee_id')
+        db_table  = 'equipment_owner'
+#        unique_together = ('equipment_id', 'employee_id')
 
