5e0bd21f4c49d6615d4641332e993791e69a3ba5
anastass <anastass@anastass-Aspire-5310>
2012-10-19 21:15:53 +0700

    deleted: model 'RightsGroupe' amended: model 'Employee'

--- techsup_run/models/Employee.py
+++ techsup_run/models/Employee.py
@@ -8,13 +8,11 @@ import settings as s
 class Employee(models.Model):
     name        = models.CharField(max_length=s.EMPLOYEE_NAME_LENGTH)
     phone       = models.CharField(max_length=s.EMPLOYEE_PHONE_LENGTH)
-    email       = models.EmailField()
     addr        = models.CharField(max_length=s.EMPLOYEE_ADDR_LENGTH)
-    login       = models.CharField(max_length=s.EMPLOYEE_LOGIN_LENGTH)    
-    password    = models.CharField(max_length=s.EMPLOYEE_PASSWORD_LENGTH)
+    login       = models.CharField(max_length=s.EMPLOYEE_LOGIN_LENGTH, null=True)    
+    password    = models.CharField(max_length=s.EMPLOYEE_PASSWORD_LENGTH, null=True)
     role        = models.ForeignKey('EmployeeRole')
-    group       = models.ForeignKey('RightsGroup')
-     
+         
     class Meta:
         app_label = 'techsup_run'
         db_table = 'employee'
