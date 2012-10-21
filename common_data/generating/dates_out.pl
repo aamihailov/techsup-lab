a1ba9d1785c474dc555258ec949cda94579c4150
aamihailov <aamihailov@aamihailov-VirtualBox.(none)>
2012-10-21 18:35:16 +0700

    add : delete_employee generation

--- common_data/generating/dates_out.pl
+++ common_data/generating/dates_out.pl
@@ -1,7 +1,21 @@
 
 
-open(dates,  "dates_out.dat");
-open(names,  "names.dat");
-open(phones, "phones.dat");
+open(snils, "snils.dat");
+open(dates, "dates_out.dat");
 
+@dates = <dates>;
+@snils = <snils>;
 
+$format = "CALL delete_employee('%s', '%s');\n";
+
+$today  = "2012-10-20";
+
+$n = scalar @snils;
+
+for ( $i = 0; $i < $n; $i++ ) {
+  $sn = @snils[$i]; chomp $sn;
+  $da = @dates[$i]; chomp $da;
+  if ( $da lt $today ) {
+    printf $format, $sn, $da;
+  }
+}
