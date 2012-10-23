fdc02cea225b87241408580721c54d670e60232f
anastass <anastass@anastass-Aspire-5310>
2012-10-24 00:39:40 +0700

    corrrected procedure for admin

--- common_data/generating/admin.sql
+++ common_data/generating/admin.sql
@@ -16,8 +16,8 @@ CALL add_equipment_and_owner( '999-999-999 99',
 
 CALL add_equipment_and_owner( '999-999-999 99',
                               'ноут шефа',
-                              'adm-999-9999-99-note',
+                              'adm-999-9999-99-laptope',
                               NULL,
-                              'рабочее место начальника',
+                              '10.1@ Ноутбук Acer Aspire One AO522-C68kk/Black (HD 1280x720) AMD C60(1.0)/2048/320/AMD HD6290/WiFi/BT/Cam/MS Win7 Starter',
                               ( SELECT NOW( ) )
                             );
