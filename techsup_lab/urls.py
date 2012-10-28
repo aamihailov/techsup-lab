0947eb6e45a076ddc1aa488de5cffb3f28aedfb3
aamihailov <aamihailov@aamihailov-VirtualBox.(none)>
2012-10-28 16:13:25 +0700

    maybe_owners procedure is done

--- techsup_lab/urls.py
+++ techsup_lab/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^(\d+)/$', 'techsup_run.views.home', name='home'),
     url(r'^(\d+)-(\d+)/$', 'techsup_run.views.many', name='home'),
+    url(r'^maybe_owners/([a-z]+-\d{3}-\d{4}-\d{2}-[a-z]+)/$', 'techsup_run.views.maybe_owners', name='home'),
 )
--- techsup_run/views.py
+++ techsup_run/views.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 
 
+from django.utils.datetime_safe import date
 from django.shortcuts import render_to_response
+from django.http import HttpResponse
 
 from techsup_run.models.Employee import Employee
-from techsup_run.models.EquipmentModel import EquipmentModel
-from techsup_run.models.EquipmentCategory import EquipmentCategory
+from techsup_run.models.Equipment import Equipment
+from techsup_run.models.EquipmentOperation import EquipmentOperation
+from techsup_run.models.EmployeeOperation import EmployeeOperation
 
+
+def get_or_none(model, **kwargs):
+    try:
+        return model.objects.get(**kwargs)
+    except model.DoesNotExist:
+        return None
 
 
 def home(request, r):
@@ -15,3 +24,32 @@ def home(request, r):
 
 def many(request, gt, lt):
     return render_to_response('test.html', {'employees_list' : Employee.objects.filter(role__gte=gt, role__lte=lt)})
+
+def maybe_owners(request, printer_id):
+    try:
+        pr = Equipment.objects.get(serial_number=printer_id)
+    except Equipment.DoesNotExist:
+        return HttpResponse()
+
+    A = EquipmentOperation.objects.get(equipment=pr, eq_oper_type_id=1).datetime.date()
+    B = get_or_none(EquipmentOperation, equipment=pr, eq_oper_type_id=2)
+    if B == None:
+        B = date('2020-12-31')
+    else:
+        B = B.datetime.date( )
+    
+    query = '''
+    SELECT * FROM employee
+    WHERE id NOT IN (
+      SELECT employee_id FROM employee_operation
+      WHERE type_id = 1 AND date < %s
+      UNION
+      SELECT employee_id FROM employee_operation
+      WHERE type_id = 2 AND date > %s
+    )'''  
+    
+    el = Employee.objects.raw(query, [B, A])
+
+    return render_to_response('test.html', {'employees_list' : el})
+    return HttpResponse('%s' % printer_id)
+
