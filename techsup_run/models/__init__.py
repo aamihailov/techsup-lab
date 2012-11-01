ee0e9f34758405a817fe9672917160695627759b
aamihailov <aamihailov@aamihailov-VirtualBox.(none)>
2012-11-02 00:21:30 +0700

    erd maked pretty

--- techsup_run/models/__init__.py
+++ techsup_run/models/__init__.py
@@ -1,24 +1,24 @@
-import Admins                   #                                    --.      |
-import DepartmentActivitySphere # --.                                  |      | f
-import Department               # <-'                 --.              |      |
-import DetailCategory           # --.                   |              |      |
-import DetailModel              # <-'                   |  --.         |      |
-import EmployeeRole             # --.                   |    |         |      | f
-import Employee                 # <-'       --.  --.    |    |  --.  <-' <-.  |
-import EmployeeOperationType    # --.         |    |    |    |    |        |  | f
-import EmployeeOperation        # <-'       <-'    |  <-'    |    |        |  |
-import EquipmentCategory        #      --.         |         |    |        |  |
-import EquipmentModel           # --.  <-'         |         |    |        |  |
-import Equipment                # <-'  --.         |         |    |        |  |
-import EquipmentOperationType   # --.    |         |         |    |        |  | f
-import EquipmentOperation       # <-'  <-'         |  --.    |    |        |  |
-import EquipmentOwner           #                  |    |    |    |        |  |
-import TaskPriority             #           --.    |    |    |    |        |  | f
-import Task                     # --.  --.  <-'  <-'    |    |    |        |  |
-import Repair                   # <-'    |  <-.       <-'  <-'    |        |  |
-import TaskState                # --.    |    |                   |        |  | f
-import TaskOperation            # <-'  <-'  --'                 <-'        |  |
-import Technics                 #                                        --'  |
+import Admins                   #                                    --.      | 1
+import DepartmentActivitySphere # --.                                  |      | 3
+import Department               # <-'                 --.              |      | 2
+import DetailCategory           # --.                   |              |      | 4
+import DetailModel              # <-'                   |  --.         |      | 5
+import EmployeeRole             # --.                   |    |         |      | 9
+import Employee                 # <-'       --.  --.    |    |  --.  <-' <-.  | 6
+import EmployeeOperationType    # --.         |    |    |    |    |        |  | 14
+import EmployeeOperation        # <-'       <-'    |  <-'    |    |        |  | 7
+import EquipmentCategory        #      --.         |         |    |        |  | 11
+import EquipmentModel           # --.  <-'         |         |    |        |  | 12
+import Equipment                # <-'  --.         |         |    |        |  | 10
+import EquipmentOperationType   # --.    |         |         |    |        |  | 14
+import EquipmentOperation       # <-'  <-'         |  --.    |    |        |  | 13
+import EquipmentOwner           #                  |    |    |    |        |  | 15
+import TaskPriority             #           --.    |    |    |    |        |  | 20
+import Task                     # --.  --.  <-'  <-'    |    |    |        |  | 17
+import Repair                   # <-'    |  <-.       <-'  <-'    |        |  | 16
+import TaskState                # --.    |    |                   |        |  | 21
+import TaskOperation            # <-'  <-'  --'                 <-'        |  | 19
+import Technics                 #                                        --'  | 22
 
 all_models = [Admins.Admins,
               Department.Department,
